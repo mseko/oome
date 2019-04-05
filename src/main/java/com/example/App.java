43526f96ae5230232201cb40b52c31d299ff4a5b
mseko <masaya.seko@nifty.ne.jp>
2019-04-05 23:37:12 +0900

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        StringBuilder sb = new StringBuilder(2000*1000*1000);
+        System.out.println( "Hello World!" );
+    }
+}
